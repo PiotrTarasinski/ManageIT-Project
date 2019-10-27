54976a6f211bc86d33e2795f481f29b47f406d0c
smuggler <tokarzewskis@gmail.com>
2019-10-27 22:41:23 +0100

    add static file handler / add avatar module

---
+++ API/src/server/api/modules/File/FileRoutes.ts
@@ -0,0 +1,60 @@
+import { documentation } from '../../../../utils';
+import * as Joi from '@hapi/joi';
+import { RouteConfig } from '../../../../typings/Hapi';
+import MainRoutes from '../../shared/routes/MainRoutes';
+import FileController from './FileController';
+
+class FileRoutes extends MainRoutes {
+  path: string = '/file';
+
+  getRoutes(): RouteConfig[] {
+    return [
+      {
+        method: 'GET',
+        path: '/avatar/{param*}',
+        handler: {
+          directory: {
+            path: './avatar',
+            redirectToSlash: true,
+            index: true
+          }
+        },
+        options: {
+          description: 'Get user avatar.',
+          auth: false,
+          tags: documentation('public', 'Avatar')
+        }
+      },
+      {
+        method: 'POST',
+        path: '/avatar',
+        options: {
+          handler: (req, res) => new FileController(req, res).setAvatar(),
+          description: 'Set or update user avatar.',
+          tags: documentation('private', 'Avatar'),
+          payload: {
+            maxBytes: 10485760,
+            output: 'stream',
+            parse: true
+          },
+          validate: {
+            payload: {
+              file: Joi.any().required()
+            }
+          }
+        }
+      },
+      {
+        method: 'DELETE',
+        path: '/avatar',
+        options: {
+          handler: (req, res) => new FileController(req, res).deleteAvatar(),
+          description: 'Delete user avatar.',
+          tags: documentation('private', 'Avatar')
+        }
+      }
+    ];
+  }
+}
+
+export default new FileRoutes();
