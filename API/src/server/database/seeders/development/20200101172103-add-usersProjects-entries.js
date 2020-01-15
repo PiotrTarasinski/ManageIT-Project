bde900ed197aebb788eb145cc77c0145d78b2924
PuerileSmuggler <tokarzewskis@gmail.com>
2020-01-15 19:15:53 +0100

    mock data additions

--- API/src/server/database/seeders/development/20200101172103-add-usersProjects-entries.js
+++ API/src/server/database/seeders/development/20200101172103-add-usersProjects-entries.js
@@ -32,6 +32,55 @@ module.exports = {
         permissions: 'Admin'
       },
       {
+        created_at: new Date(2019, 11, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e0d71f75-c32c-4c61-9879-6d65f353d3ab',
+        project_id: '9c620dc5-ec2f-43b1-9b1e-92c7ce01a45f',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 11, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e0d71f75-c32c-4c61-9879-6d65f353d3ab',
+        project_id: '08f416f5-bcd7-4e16-8168-09a1516ffa35',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 11, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e0d71f75-c32c-4c61-9879-6d65f353d3ab',
+        project_id: 'c1b83d0e-180b-4e21-b545-d0f6ac6e2648',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 11, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e0d71f75-c32c-4c61-9879-6d65f353d3ab',
+        project_id: '03a39404-6015-4cf1-b5b8-c9cf138989ba',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 11, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e0d71f75-c32c-4c61-9879-6d65f353d3ab',
+        project_id: 'c9858f7d-3dfc-4ef1-a85f-42f18c1207fc',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 11, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e0d71f75-c32c-4c61-9879-6d65f353d3ab',
+        project_id: '04baeae5-37ad-4f89-aae4-1e7a48ba3504',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 11, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e0d71f75-c32c-4c61-9879-6d65f353d3ab',
+        project_id: '4783c6f9-1e63-4958-968e-7dd6a5b02915',
+        permissions: 'User'
+      },
+      {
         created_at: new Date(2010, 9, 1),
         updated_at: new Date(2019, 12, 1),
         user_id: '08fcba32-23b9-4678-ba4a-bc688b6472c1',
@@ -49,6 +98,41 @@ module.exports = {
         created_at: new Date(2019, 12, 1),
         updated_at: new Date(2019, 12, 1),
         user_id: '544dda84-8986-405c-8135-b0da3b956f0c',
+        project_id: 'a6a02365-7052-4236-8894-ac594da9c108',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 12, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'bab49418-7c3b-4dec-8120-c41648ff18cb',
+        project_id: 'a6a02365-7052-4236-8894-ac594da9c108',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 12, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'fd179a7c-3059-4286-9e8e-52a133460709',
+        project_id: 'a6a02365-7052-4236-8894-ac594da9c108',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 12, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'a21c87bb-1ef1-4074-a302-72808e3efa5e',
+        project_id: 'a6a02365-7052-4236-8894-ac594da9c108',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 12, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: 'e894430a-9aa9-4e80-8302-4ce16a2550ec',
+        project_id: 'a6a02365-7052-4236-8894-ac594da9c108',
+        permissions: 'User'
+      },
+      {
+        created_at: new Date(2019, 12, 1),
+        updated_at: new Date(2019, 12, 1),
+        user_id: '9c620dc5-ec2f-43b1-9b1e-92c7ce01a45f',
         project_id: 'a6a02365-7052-4236-8894-ac594da9c108',
         permissions: 'User'
       }
